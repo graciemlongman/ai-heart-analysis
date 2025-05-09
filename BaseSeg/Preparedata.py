81e12aa9bc5a67742b140a6e498a4a8d01ef3811
nc0051 <nc0051@lnx-n109-12.lunet.lboro.ac.uk>
2025-05-09 21:48:17 +0100

    mostly working prepare datasets scripts for both baseg tasks

--- BaseSeg/Preparedata.py
+++ BaseSeg/Preparedata.py
@@ -4,7 +4,8 @@ import shutil
 import numpy as np
 from collections import defaultdict
 import sys 
-sys.path.append(os.path.expanduser('~/PROJECT/'))
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
 from Preprocess import preprocess_inplace
 
 
@@ -51,9 +52,11 @@ def classify_l_r(file_store):
                 continue
     return label_store
 
-def seg_prepare_data(label_store, dataset_dir='arcade/syntax/'):
+def seg_prepare_data(dataset_dir='arcade/syntax/'):
     
     file_store = load_annotations(dataset_dir)
+    label_store = classify_l_r(file_store)
+    print(label_store['train',922][0]=='LCA')
     
     dataset_newL= 'BaseSeg/syntaxLCA'
     dataset_newR='BaseSeg/syntaxRCA'
@@ -68,8 +71,8 @@ def seg_prepare_data(label_store, dataset_dir='arcade/syntax/'):
                     os.makedirs(f'{dataset_new}/{type}/{split}/')
     
     # adapted from https://github.com/cmctec/ARCADE/blob/main/useful%20scripts/coco_to_yolo.ipynb
-    name_anns=defaultdict(list)
     for split, file in file_store.items():
+        name_anns=defaultdict(list)
         name_cls= {img['id']: img['file_name'] for img in file['images']}
         #format the annotations
         for ann in file['annotations']:
@@ -80,7 +83,8 @@ def seg_prepare_data(label_store, dataset_dir='arcade/syntax/'):
         
         for k, v in name_anns.items():
             id = os.path.splitext(k)[0]
-            if label_store[split, id] == 'LCA':
+            print(split, int(id))
+            if label_store[str(split), int(id)][0] == 'LCA':
                 with open(f"{f'{dataset_newL}/labels/{split}/{id}.txt'}", "w", encoding="utf-8") as file:
                     file.write("\n".join(v))
             else:
@@ -91,7 +95,8 @@ def seg_prepare_data(label_store, dataset_dir='arcade/syntax/'):
         #copy images into correct file structures
         for img_id in name_cls.values():
             src = f'{dataset_dir}{split}/images/{img_id}'
-            if label_store[split, img_id] == 'LCA':
+            print('img_id:',img_id[:-4])
+            if label_store[split, int(img_id[:-4])][0] == 'LCA':
                 dst = f'{dataset_newL}/images/{split}/'
                 copy_image(src,dst)
             else:
@@ -131,4 +136,6 @@ def cls_prepare_data(dataset_dir='arcade/syntax/', dataset_new='BaseSeg/syntax1'
 if __name__ == '__main__':
     file_store=load_annotations(dataset_dir='arcade/syntax/')
     label_store = classify_l_r(file_store=file_store)
-    seg_prepare_data(label_store)
+    #print(label_store)
+    seg_prepare_data()
+
